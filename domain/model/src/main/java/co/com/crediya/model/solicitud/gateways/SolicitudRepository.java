94f40f89740ba9f4013feb9ea69c0180d45b52d1
Alexis Pajuelo Cieza <pajueloa@royalsystems.net>
2025-08-24 14:10:25 -0500

    adding methods in repository and usercase for solicitud

--- domain/model/src/main/java/co/com/crediya/model/solicitud/gateways/SolicitudRepository.java
+++ domain/model/src/main/java/co/com/crediya/model/solicitud/gateways/SolicitudRepository.java
@@ -1,4 +1,14 @@
 package co.com.crediya.model.solicitud.gateways;
 
+import co.com.crediya.model.solicitud.Solicitud;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface SolicitudRepository {
+
+    Flux<Solicitud> findAll();
+
+    Mono<Solicitud> save(Solicitud solicitud);
+
+    Mono<Solicitud> findById(Integer id);
 }
--- domain/usecase/src/main/java/co/com/crediya/usecase/solicitud/SolicitudUseCase.java
+++ domain/usecase/src/main/java/co/com/crediya/usecase/solicitud/SolicitudUseCase.java
@@ -1,6 +1,25 @@
 package co.com.crediya.usecase.solicitud;
 
+import co.com.crediya.model.solicitud.Solicitud;
+import co.com.crediya.model.solicitud.gateways.SolicitudRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @RequiredArgsConstructor
 public class SolicitudUseCase {
+
+    private final SolicitudRepository solicitudRepository;
+
+    public Flux<Solicitud> getAllSolicitudes() {
+        return solicitudRepository.findAll();
+    }
+
+    public Mono<Solicitud> saveSolicitud(Solicitud solicitud) {
+        return solicitudRepository.save(solicitud);
+    }
+
+    public Mono<Solicitud> findSolicitudById(Integer id) {
+        return solicitudRepository.findById(id);
+    }
 }
