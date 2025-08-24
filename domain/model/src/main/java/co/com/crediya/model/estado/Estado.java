9d25cbef69450bec78a45b6ab13de05e76c603ab
Alexis Pajuelo Cieza <pajueloa@royalsystems.net>
2025-08-24 14:01:54 -0500

    adding attributes for Estado and TipoPrestamo model

--- domain/model/src/main/java/co/com/crediya/model/estado/Estado.java
+++ domain/model/src/main/java/co/com/crediya/model/estado/Estado.java
@@ -2,13 +2,17 @@ package co.com.crediya.model.estado;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class Estado {
+
+    private Integer id_estado;
+    private String nombre;
+    private String descripcion;
 }
--- domain/model/src/main/java/co/com/crediya/model/tipoprestamo/TipoPrestamo.java
+++ domain/model/src/main/java/co/com/crediya/model/tipoprestamo/TipoPrestamo.java
@@ -2,13 +2,22 @@ package co.com.crediya.model.tipoprestamo;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class TipoPrestamo {
+
+    private Integer id_tipo_prestamo;
+    private String nombre;
+    private BigDecimal monto_minimo;
+    private BigDecimal monto_maximo;
+    private BigDecimal tasa_interes;
+    private Boolean validacion_automatica;
 }
