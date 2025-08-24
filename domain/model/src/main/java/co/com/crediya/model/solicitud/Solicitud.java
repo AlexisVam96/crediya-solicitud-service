eccea309f57e5861acb009c649fab93ab12e0e5c
Alexis Pajuelo Cieza <pajueloa@royalsystems.net>
2025-08-24 13:55:57 -0500

    implement atributes in Solicitud

--- domain/model/src/main/java/co/com/crediya/model/solicitud/Solicitud.java
+++ domain/model/src/main/java/co/com/crediya/model/solicitud/Solicitud.java
@@ -2,13 +2,22 @@ package co.com.crediya.model.solicitud;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class Solicitud {
+
+    private Integer id_solicitud;
+    private BigDecimal monto;
+    private Integer plazo;
+    private String email;
+    private Integer id_estado;
+    private Integer id_tipo_prestamo;
 }
