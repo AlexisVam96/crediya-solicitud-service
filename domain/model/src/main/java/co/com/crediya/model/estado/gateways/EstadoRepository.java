c20535f44ecfc2ab53ba69f6e18d0a75d894d8b3
Alexis Pajuelo Cieza <pajueloa@royalsystems.net>
2025-08-24 19:32:19 -0500

    implement method for Estado and TipoPrestamo repository

--- domain/model/src/main/java/co/com/crediya/model/estado/gateways/EstadoRepository.java
+++ domain/model/src/main/java/co/com/crediya/model/estado/gateways/EstadoRepository.java
@@ -1,4 +1,11 @@
 package co.com.crediya.model.estado.gateways;
 
+import co.com.crediya.model.estado.Estado;
+import reactor.core.publisher.Mono;
+
 public interface EstadoRepository {
+
+    Mono<Estado> findById(Integer id);
+
+    Mono<Estado> findByNombre(String nombre);
 }
